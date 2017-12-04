eed80315353d5f6b2049ece57b7f0f14a7878456
Jay Tucker <jtucker@imprivata.com>
2017-12-04 12:33:46 -0500

    Initial commit

---
+++ ConnectionTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectionTester
+//
+//  Created by Jay Tucker on 12/4/17.
+//  Copyright © 2017 Imprivata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
